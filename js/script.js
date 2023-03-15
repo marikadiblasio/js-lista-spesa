125c5f757b9dc4c8786afd911762450e1459d137
marikadiblasio <marbla.albram@gmail.com>
2023-03-15 13:13:10 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,3 @@
+/*
+Rivediamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+*/
